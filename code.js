df8ec76235571dd90baa799364857cfe8d69c63e
Dominik Prinzensteiner <d.prinzensteiner@gmail.com>
2017-10-05 15:07:38 +0200

    init commit

---
+++ code.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+  $.get(
+      "https://codestats.net/api/users/datguy",
+      {paramOne : 1, paramX : 'abc'},
+      function(data) {
+         animateData(data);
+      }
+  );
+});
+
+function animateData(data) {
+  $username = data.user;
+  $totaluserXP = data.total_xp;
+  $totalusernewXP = data.new_xp;
+  $userMaschines = data.maschines;
+  $userLangs = data.languages;
+
+  $totalLevel = get_level($totaluserXP);
+  $totalNextLevelXP = get_next_level_xp($totalLevel);
+
+  $("#username").text($("#username").text() + $username);
+
+  $("#total_xp").text($("#total_xp").text() + $totalLevel);
+  var total_xp_container = new ProgressBar.Circle('#total_xp_container', {
+        color: '#aefc3d',
+        trailWidth: 10,
+        strokeWidth: 10,
+        duration: 3000,
+        easing: 'easeInOut',
+        text: {
+          value: $totaluserXP
+        }
+    });
+  $("#new_xp").text($("#new_xp").text() + $totalusernewXP);
+
+  total_xp_container.animate($totaluserXP / $totalNextLevelXP);
+
+}
+
+function get_level(xp) {
+  return Math.floor(0.025 * Math.sqrt(xp));
+}
+
+function get_next_level_xp(level) {
+  return Math.pow(Math.ceil((level + 1) / 0.025), 2)
+}
